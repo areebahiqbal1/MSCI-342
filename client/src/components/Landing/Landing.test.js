f09161d70a0b62a52fb40ede765f878c288d0fcd
areebahiqbal <68248736+areebahiqbal@users.noreply.github.com>
2023-03-03 11:53:14 -0500

    cypress test

--- client/src/components/Landing/Landing.test.js
+++ client/src/components/Landing/Landing.test.js
@@ -2,11 +2,10 @@ import "@testing-library/jest-dom";
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import TheLanding from "./Landing";
+import cy from 'cypress';
 
 
-
-
-jest.mock('./resumee.jpg', () => ({})); // Mock the resumee.jpg file
+jest.mock('./resumee.jpg', () => ({})); // Mocks the resumee.jpg file
 
 describe('TheLanding component', () => {
   it('renders the landing page title', () => {
@@ -14,25 +13,6 @@ describe('TheLanding component', () => {
     const titleElement = getByText('Can do Co-op');
     expect(titleElement).toBeInTheDocument();
   });
-
-  // Add additional test cases as needed
-});
-
-
-// module.exports = {
-//     // other configuration options...
   
-//     moduleNameMapper: {
-//       "\\.(jpg|jpeg|png|gif)$": "client/__mocks__/resumee.jpg",
-//     },
-//   };
-//  test("renders the landing page title", () => {
-//    const { getByText } = render(<TheLanding />);
-//    const titleElement = getByText("Can do Co-op");
-//    expect(titleElement).toBeInTheDocument();
-//  });
-
-// it("renders Landing component", () => {
-//   render(<TheLanding />);
-//   expect(screen.getByText("Can do Co-op")).toBeInTheDocument();
-// });
+  
+});

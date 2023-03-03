84fc72d18849005cb2d8876b88158127eeb4564c
areebahiqbal <68248736+areebahiqbal@users.noreply.github.com>
2023-03-03 01:47:27 -0500

    test with mock image

--- client/src/components/Landing/Landing.test.js
+++ client/src/components/Landing/Landing.test.js
@@ -3,13 +3,36 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import TheLanding from "./Landing";
 
-test("renders the landing page title", () => {
-  const { getByText } = render(<TheLanding />);
-  const titleElement = getByText("Can do Co-op");
-  expect(titleElement).toBeInTheDocument();
+
+
+
+jest.mock('./resumee.jpg', () => ({})); // Mock the resumee.jpg file
+
+describe('TheLanding component', () => {
+  it('renders the landing page title', () => {
+    const { getByText } = render(<TheLanding />);
+    const titleElement = getByText('Can do Co-op');
+    expect(titleElement).toBeInTheDocument();
+  });
+
+  // Add additional test cases as needed
 });
 
-it("renders Landing component", () => {
-  render(<TheLanding />);
-  expect(screen.getByText("Can do Co-op")).toBeInTheDocument();
-});
+
+// module.exports = {
+//     // other configuration options...
+  
+//     moduleNameMapper: {
+//       "\\.(jpg|jpeg|png|gif)$": "client/__mocks__/resumee.jpg",
+//     },
+//   };
+//  test("renders the landing page title", () => {
+//    const { getByText } = render(<TheLanding />);
+//    const titleElement = getByText("Can do Co-op");
+//    expect(titleElement).toBeInTheDocument();
+//  });
+
+// it("renders Landing component", () => {
+//   render(<TheLanding />);
+//   expect(screen.getByText("Can do Co-op")).toBeInTheDocument();
+// });

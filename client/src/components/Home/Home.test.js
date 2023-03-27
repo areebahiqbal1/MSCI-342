38bb8d5225987f457fd6968aa938fa52528b706d
areebahiqbal <68248736+areebahiqbal@users.noreply.github.com>
2023-03-27 14:06:56 -0400

    updated version of FAQ, and tests

---
+++ client/src/components/Home/Home.test.js
@@ -0,0 +1,19 @@
+import "@testing-library/jest-dom";
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from 'react-router-dom';
+
+import Home from "./index";
+import { createTheme } from "@material-ui/core/styles";
+
+describe('Home component', () => {
+    it('renders the Home page title', () => {
+      const { getByText } = render(
+        <MemoryRouter>
+        <Home />
+      </MemoryRouter>
+      );
+      const titleElement = getByText('CAN-DO-CO-OP');
+      expect(titleElement).toBeInTheDocument();
+    });
+});

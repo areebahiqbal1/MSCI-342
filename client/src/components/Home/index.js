58319560c0754ed23d9da930c3936ee405145f4e
areebahiqbal <68248736+areebahiqbal@users.noreply.github.com>
2023-03-27 11:19:31 -0400

    calendar saved

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -12,7 +12,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Container from '@material-ui/core/Container';
 import Toolbar from '@material-ui/core/Toolbar';
 import './Home.css';
-import MenuBar from '../MenuBar/menu';
+
 
 const serverURL = "";
 
@@ -110,7 +110,7 @@ class HomeBase extends React.Component {
     this.setState({ mobileMoreAnchorEl: null });
     this.props.firebase.doSignOut();
     this.props.history.push("/");
-  }
+  }
 
   render() {
     return (
@@ -123,151 +123,131 @@ class HomeBase extends React.Component {
             overflow: 'scroll',
             backgroundImage: `url(https://source.unsplash.com/q10VITrVYUM)`,
             backgroundSize: "cover"
-          }}
-        >
-          <AppBar position="static">
-            <Container maxWidth="xl">
-              <Toolbar disableGutters>
-                <Button
-                  key='1'
-                  onClick={() => history.push('/')}
-                  sx={{ my: 2, color: 'white', display: 'block' }}
-                >
-                  Home
-                </Button>
-                <Button
-                  key='2'
-                  onClick={() => history.push('/MyFiles')}
-                  sx={{ my: 2, color: 'white', display: 'block' }}
-                >
-                  My Files
-                </Button>
-                <Button
-                  key='3'
-                  onClick={() => history.push('/Upload')}
-                  sx={{ my: 2, color: 'white', display: 'block' }}
-                >
-                  Upload
-                </Button>
-                <Button
-                  key='4'
-                  onClick={() => history.push('/Profile')}
-                  sx={{ my: 2, color: 'white', display: 'block' }}
-                >
-                  Profile
-                </Button>
-                <Button
-                  key='4'
-                  onClick={() => history.push('/Calendar')}
-                  sx={{ my: 2, color: 'white', display: 'block' }}
-                >
-                  Calendar
-                </Button>
-                <Button
-                  key='7'
-                  onClick={() => history.push('/About')}
-                  sx={{ my: 2, color: 'red', display: 'block' }}
-                >
-                  About
-                </Button>
-                <Button
-                  key='8'
-                  onClick={() => history.push('/FAQ')}
-                  sx={{ my: 2, color: 'red', display: 'block' }}
-                >
-                  FAQ
-                </Button>
-                <Button
-                  key='9'
-                  onClick={() => history.push('/Apply')}
-                  sx={{ my: 2, color: 'red', display: 'block' }}
-                >
-                  Apply
-                </Button>
-                <Button
-                  key='10'
-                  onClick={() => history.push('/Review')}
-                  sx={{ my: 2, color: 'red', display: 'block' }}
-                >
-                  Review
-                </Button>
-                <Button
-                  key='11'
-                  onClick={() => history.push('/Admin')}
-                  sx={{ my: 2, color: 'red', display: 'block' }}
-                >
-                  Admin
-                </Button>
-                <Button
-                  key='5'
-                  onClick={this.signOut.bind(this)}
-                  sx={{ my: 2, color: 'white', display: 'block' }}
-                >
-                  SignOut
-                </Button>
-              </Toolbar>
-            </Container>
-          </AppBar>
-          <Box
-            sx={{
-              mt: 15,
-              mx: 20,
-              px: 4,
-              py: 10,
-              backgroundColor: "background.paper",
-              borderRadius: 1,
-              boxShadow: 5,
-            }}
-          >
-            <Grid container spacing={6} justifyContent="center">
-              <Grid item>
-                <Typography
-                  class="can-do-co-op"
-                  variant="h1"
-                  component="h1"
-                  align="center"
-                  gutterBottom
-                  style={{
-                    color: '#333',
-                    fontSize: '4rem',
-                    fontWeight: 'bold',
-                    textShadow: '2px 2px #ccc',
-                    letterSpacing: '0.1em',
-                    lineHeight: '1.2',
-                  }}
-                >
-                  CAN-DO-CO-OP
-                  <br></br><br></br>
-                </Typography>
-              </Grid>
-            </Grid>
-            <div style={{ border: '3px solid #ccc', padding: '0.5rem', backgroundColor: '#f5f5f5', animation: 'fade-in 3s forwards' }}>
-              <Typography
-                variant="h1"
-                component="h1"
-                align="center"
-                gutterBottom
-                style={{
-                  color: '#000',
-                  fontSize: '1.5rem',
-                  letterSpacing: '0.1em',
-                  lineHeight: '1.2',
-                }}
+          }}
+        >
+        <AppBar position="static">
+          <Container maxWidth="xl">
+            <Toolbar disableGutters>
+              <Button
+                key='1'
+                onClick={() => history.push('/')}
+                sx={{ my: 2, color: 'white', display: 'block' }}
               >
-                Can Do Co-op is a website that connects students with experienced reviewers to get feedback on their <br></br>cover letters, resumes, and work term reports. <br></br>It's a user-friendly platform with helpful resources and tips for students to improve their job search skills.
-              </Typography>
-            </div>
-            <Grid container
-              direction="column"
-              judtifyContent="center"
-              alignItems="center">
+                Home
+              </Button>
+              <Button
+                key='2'
+                onClick={() => history.push('/MyFiles')}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                My Files
+              </Button>
+              <Button
+                key='3'
+                onClick={() => history.push('/Upload')}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                Upload
+              </Button>
+              <Button
+                key='4'
+                onClick={() => history.push('/Profile')}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                Profile
+              </Button>
+              <Button
+                key='5'
+                onClick={this.signOut.bind(this)}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                SignOut
+              </Button>
+              <Button
+                key='6'
+                onClick={() => history.push('/Review')}
+                sx={{ my: 2, color: 'red', display: 'block' }}
+              >
+                Review
+              </Button>
+              <Button
+                key='6'
+                onClick={() => history.push('/Calendar')}
+                sx={{ my: 2, color: 'red', display: 'block' }}
+              >
+                Calendar
+              </Button>
+              <Button
+                key='6'
+                onClick={() => history.push('/Admin')}
+                sx={{ my: 2, color: 'red', display: 'block' }}
+              >
+                Admin
+              </Button>
+              
+            </Toolbar>
+          </Container>
+        </AppBar>
+        <Box
+        sx={{
+          mt: 15,
+          mx: 20,
+          px: 4,
+          py: 10,
+          backgroundColor: "background.paper",
+          borderRadius: 1,
+          boxShadow: 5,
+        }}
+      >
+<Grid container spacing={6} justifyContent="center">
+  <Grid item>
+    <Typography
+        class="can-do-co-op"
+        variant="h1"
+        component="h1"
+        align="center"
+        gutterBottom
+        style={{
+        color: '#333', 
+        fontSize: '4rem', 
+        fontWeight: 'bold', 
+        textShadow: '2px 2px #ccc', 
+        letterSpacing: '0.1em', 
+        lineHeight: '1.2', 
+      }}
+    >
+      CAN-DO-CO-OP
+      <br></br><br></br>
+    </Typography>
+  </Grid>
+</Grid>
+<div style={{ border: '3px solid #ccc', padding: '0.5rem', backgroundColor: '#f5f5f5', animation: 'fade-in 3s forwards' }}>
+  <Typography
+    variant="h1"
+    component="h1"
+    align="center"
+    gutterBottom
+    style={{
+      color: '#000', 
+      fontSize: '1.5rem', 
+      letterSpacing: '0.1em', 
+      lineHeight: '1.2', 
+    }}
+  >
+    Can Do Co-op is a website that connects students with experienced reviewers to get feedback on their cover letters, resumes, and work term reports. <br></br>It's a user-friendly platform with helpful resources and tips for students to improve their job search skills.
+  </Typography>
+</div>
+<Grid container
+direction="column"
+judtifyContent="center"
+alignItems="center">
 
-            </Grid>
-          </Box>
-        </Box>
+   </Grid>
+    </Box>
+    </Box>
 
-      </ThemeProvider>
-    );
+  </ThemeProvider>
+);
   }
 }
 
--- client/src/components/Navigation/PrivateRoute.js
+++ client/src/components/Navigation/PrivateRoute.js
@@ -5,7 +5,7 @@ import SignInForm from "../SignIn/SignIn";
 import UploadForm from "../Upload";
 import ReviewForm from "../Review";
 import ViewPage from "../View";
-import CalendarPage from "../Calendar/Calendar";
+import CalendarPage from "../Calendar";
 import AdminPage from "../Admin";
 import AdminView from "../AdminView";
 import MyFilesPage from "../MyFiles";
